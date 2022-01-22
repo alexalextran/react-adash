c4c44aacff4fabc6372606b19bcf5c915aec4bbf
alexalextran <alextran8385@gmail.com>
2022-01-23 00:35:39 +1100

    carousel framework done

--- src/components/YTvideos.jsx
+++ src/components/YTvideos.jsx
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 const Ytvideos = () => {
-    return (
-        <section id="YTvideos">
-        nice
-        </section>
-    );
-}
+  function carousel() {
+      
+    document.querySelectorAll(".carousel").forEach((carousel) => {
+        const items = carousel.querySelectorAll(".carousel__item");
+        const buttonsHtml = Array.from(items, () => {
+          return `<span class="carousel__button"></span>`;
+        });
+      
+        carousel.insertAdjacentHTML(
+          "beforeend",
+          `
+              <div class="carousel__nav">
+                  ${buttonsHtml.join("")}
+              </div>
+          `
+        );
+      
+        const buttons = carousel.querySelectorAll(".carousel__button");
+      
+        buttons.forEach((button, i) => {
+          button.addEventListener("click", () => {
+            // un-select all the items
+            items.forEach((item) =>
+              item.classList.remove("carousel__item--selected")
+            );
+            buttons.forEach((button) =>
+              button.classList.remove("carousel__button--selected")
+            );
+      
+            items[i].classList.add("carousel__item--selected");
+            button.classList.add("carousel__button--selected");
+          });
+        });
+      
+        // Select the first item on page load
+        items[0].classList.add("carousel__item--selected");
+        buttons[0].classList.add("carousel__button--selected");
+      });
+      
+  }
+
+  useEffect(() => {
+    carousel();
+  }, []);
+
+  return (
+    <section id="YTvideos">
+      <div className="carousel">
+        <div className="carousel__item">1</div>
+        <div className="carousel__item carousel__item--selected">2</div>
+        <div className="carousel__item">3</div>
+      </div>
+    </section>
+  );
+};
 
 export default Ytvideos;
