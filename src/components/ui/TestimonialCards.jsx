5fcb8cc8436916e8f0dbc7261ca07e22fc8b44b7
alexalextran <alextran8385@gmail.com>
2022-01-26 12:00:00 +1100

    responsive for large and small laptops

--- src/components/ui/TestimonialCards.jsx
+++ src/components/ui/TestimonialCards.jsx
@@ -4,9 +4,12 @@ const Testimonialcards = (testi) => {
     return (
         <div className='testimonial__cards'>
         <div className='image__wrapper'><img src={testi.img} className='testimonial__profilepic'/></div>
-        <p>{testi.para}</p>
-        <h3 className='testimonial__name'>{testi.name}</h3>
-        <p>{testi.title}</p>
+        <p className="whyus__card--para">{testi.para}</p>
+        <div className='testimonial__details'>
+             <h3 className='testimonial__name'>{testi.name}</h3>
+        <p  >{testi.title}</p>
+        </div>
+       
         </div>
     );
 }
